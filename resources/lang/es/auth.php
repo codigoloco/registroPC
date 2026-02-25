829640df8b4910cf81cd61a8ef4fd80a1130fbab
devkmendoza27 <dev.kmendoza@gmail.com>
2026-02-25 11:11:33 -0400

    Se corrigen detalles de session

--- resources/lang/es/auth.php
+++ resources/lang/es/auth.php
@@ -1,7 +1,7 @@
 <?php
 
 return [
-    'failed'   => 'Estas credenciales no coinciden con nuestros registros.',
+    'failed' => 'Estas credenciales no coinciden con nuestros registros.',
     'password' => 'La contraseña proporcionada es incorrecta.',
-    'throttle' => 'Demasiados intentos de inicio de sesión. Por favor intenta de nuevo en :seconds segundos.',
+    'throttle' => 'Usuario Bloqueado por demasiados intentos fallidos, comunicarse con su supervisor.',
 ];
